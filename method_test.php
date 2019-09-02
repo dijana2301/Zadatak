5fb96b37b2085a51efc533bcbae8dce4d4b9ff54
dijana2301 <dijanak_93@yahoo.com>
2019-09-02 15:49:26 +0200

    Izmjene na metohod_test

---
+++ method_test.php
@@ -0,0 +1,16 @@
+<form action="method_test.php?var=value" method="POST">
+<input name="var" value="value1">
+<input type="submit">
+</form>
+
+<?php
+echo "<h1>GET:</h1>";
+var_dump($_GET);
+
+echo "<h1>POST:</h1>";
+var_dump($_POST);
+
+echo "<h1>SERVER:</h1>";
+var_dump($_SERVER);
+
+echo "...";
