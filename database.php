5a18d87d771888ef2aded77ac8f2e9f104aca170
dijana2301 <54356959+dijana2301@users.noreply.github.com>
2019-09-09 12:23:00 +0200

    Delete database.php

--- database.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbServerName = "localhost:8080";
-$dbUserName = "root";
-$dbPass = "";
-$dbName = "glassesrus";
-
-$conn = mysqli_connect($dbServerName, $dbUserName, $dbPass, $dbName);
-echo $conn;
-?>
